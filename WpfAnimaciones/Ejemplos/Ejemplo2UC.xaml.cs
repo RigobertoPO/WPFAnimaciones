5e8b376eba04ceece0255019a1c375274b02ac6e
RigobertoPO <rperez@cobach.edu.mx>
2017-06-01 13:06:08 -0500

    Version 5, incluye ejemplo 2

--- WpfAnimaciones/Ejemplos/Ejemplo2UC.xaml.cs
+++ WpfAnimaciones/Ejemplos/Ejemplo2UC.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,27 @@ namespace WpfAnimaciones.Ejemplos
         public Ejemplo2UC()
         {
             InitializeComponent();
+        }
+        public bool  fueEnviado { get; set; }
+
+        private void EnviarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (fueEnviado == false)
+            {
+                fueEnviado = true;
+                Storyboard s = (Storyboard)TryFindResource("EnviarStoryboard");
+                s.Begin();
+            }
+        }
+
+        private void RegresarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (fueEnviado == true)
+            {
+                fueEnviado = false;
+                Storyboard s = (Storyboard)TryFindResource("RegresarStoryboard");
+                s.Begin();
+            }
         }
     }
 }
